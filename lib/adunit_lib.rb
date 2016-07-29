21cff241183c47475056259f6fe8671244c26121
Aleksandr Makarenko <sanjcho@gmail.com>
2016-07-29 16:43:30 +0500

    some very draft version

--- lib/adunit_lib.rb
+++ lib/adunit_lib.rb
@@ -4,18 +4,21 @@ require "watir-webdriver"
 
 class AdunitAdd
   def inicialize
-  	login
+    login
+    puts "hello"
   end
 
   def self.call
-  	new
+    new.login
   end
 
   def login
-  	browser = Watir::Browser.new
-	browser.goto 'https://target.my.com/'
-	browser.text_field(:name, "login").set("aleksmakarenko@inbox.ru")
-	browser.text_field(:name, "password").set("h393co02")
-	browser.button(:class, "button_submit button").click
+    browser = Watir::Browser.start('https://target.my.com/#login')
+    sleep(1) if browser.status != "Done"
+    browser.text_field(:name, "login").set("aleksmakar@inbox.ru")
+    browser.text_field(:name, "password").set("h393co02")
+    browser.button(:class, "button_submit button").click
+    sleep(1) if browser.status != "Done" && browser.text.include?("You have no active campaigns. Create one?") == true 
+    puts "hellow"
   end
 end
